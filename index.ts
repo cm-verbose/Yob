041abceaf1948a19921fb741eb43a06891f056a4
vb <68516521+cm-verbose@users.noreply.github.com>
2023-10-15 21:55:30 -0400

    Added logging for editing, fixed small edit bugs

--- index.ts
+++ index.ts
@@ -1,25 +1,32 @@
 import {
+  APIEmbed,
+  APIEmbedField,
   Client,
-  GatewayIntentBits as GIB,
   EmbedBuilder,
   Events,
+  GatewayIntentBits as GIB,
+  Guild,
   Message,
   PartialMessage,
-  User,
-  TextChannel,
   PermissionsBitField,
+  TextChannel,
+  User,
 } from "discord.js";
 import dotenv from "dotenv";
 
 class Bot {
   client: Client;
   logChannelID: string;
+  DISCORD_EPOCH: number;
+  PREFIX: string;
 
   constructor() {
     /** @important Reminder : Do not change these */
     this.client = new Client({
-      intents: [GIB.Guilds, GIB.GuildPresences, GIB.GuildMembers, GIB.GuildMessages, GIB.MessageContent],
+      intents: [GIB.Guilds, GIB.GuildMembers, GIB.GuildMessages, GIB.GuildPresences, GIB.MessageContent],
     });
+    this.PREFIX = "~ ";
+    this.DISCORD_EPOCH = 1420070400000;
     this.logChannelID = "";
     this.ini();
   }
@@ -27,6 +34,7 @@ class Bot {
   /** @description Constructs cient */
   private ini() {
     this.handleReady();
+    this.handleMessageUpdate();
     this.handleMessageSend();
     this.handleMessageDelete();
     this.handleLogin();
@@ -46,9 +54,11 @@ class Bot {
       if (message.content === null) return;
 
       /** @description configures the log channel */
-      if (message.content === "~ setLogChannel") {
-        if(message.author?.bot) return; 
-        if(!message.member || !(message.member?.permissions.has(PermissionsBitField.Flags.BanMembers))) return; 
+      if (message.content === `${this.PREFIX}setLogChannel`) {
+        if (message.author?.bot) return;
+
+        /** @description Base permissions */
+        if (!message.member || !message.member?.permissions.has(PermissionsBitField.Flags.BanMembers)) return;
 
         this.logChannelID = message.channel.id;
         const embed = new EmbedBuilder().setTitle(`Set log channel as <#${this.logChannelID}>`).setColor(0x77ff77);
@@ -60,65 +70,155 @@ class Bot {
   /** @description Handles deleted messages */
   private handleMessageDelete() {
     this.client.on(Events.MessageDelete, async (message: Message<boolean> | PartialMessage) => {
+      /** If no log channel is configured, there is no location to log */
       if (this.logChannelID === "") return;
 
-      const messageContent: string = message.content !== null ? message.content : "";
-      const messageAuthourProfile = (message.author as User).avatarURL({ size: 64 }) as string;
+      const messageContent: string | null = message.content;
+      const messageAuthor = message.author as User;
+      const messageAuthourProfile = messageAuthor.avatarURL({ size: 64 }) as string;
       const messageId: string = message.id;
 
-      const fieldList: Array<{ name: string; value: string; inline?: boolean }> = [];
+      const fieldList: Array<APIEmbedField> = [
+        { name: "Message ID", value: `\`${messageId}\``, inline: true },
+        {
+          name: "Message Timestamp",
+          value: `<t:${this.convertSnowFlakeToDate(parseInt(messageId, 10))}:F>`,
+          inline: true,
+        },
+        {
+          name: "User",
+          value: `\`${messageAuthor.id}\` - <@${messageAuthor.id}>`,
+          inline: true,
+        },
+      ];
       const attachementList: Array<string> = [];
-
-      fieldList.push({ name: "Message ID", value: `\`${messageId}\``, inline: true });
-      fieldList.push({
-        name: "Message Timestamp",
-        value: `<t:${this.convertSnowFlakeToDate(parseInt(messageId, 10))}:F>`,
-        inline: true,
-      });
-      fieldList.push({
-        name: "User",
-        value: `\`${(message.author as User).id}\` (<@${(message.author as User).id}>)`,
-        inline: true,
-      });
-
-      const embedColor = 0xff7777;
+      const EMBED_COLOR: number = 0xff7777;
       const logEmbed = {
         author: {
-          name: `${(message.author as User).globalName as string}`,
+          name: messageAuthor.globalName ?? messageAuthor.username,
           iconURL: messageAuthourProfile,
         },
-        color: embedColor,
+        color: EMBED_COLOR,
         title: `Message deleted in <#${message.channelId}>`,
-        description: messageContent === "" ? "<message content empty>" : `\`\`\`md\n${messageContent}\`\`\``,
+        description:
+          messageContent === null
+            ? message.embeds.length >= 1
+              ? `<see embed${message.embeds.length > 1 ? "s" : ""} below>`
+              : "<message content empty>"
+            : `\`\`\`md\n${messageContent}\`\`\``,
         fields: fieldList,
-      };
+        type: "rich",
+      } as APIEmbed;
+
+      const embedArray: APIEmbed[] = [logEmbed];
+
+      if (message.embeds.length >= 1) {
+        message.embeds.forEach((embed) => {
+          const embedObject = embed.data;
+          const createdEmbed: APIEmbed = this.restructureEmbedObject(embedObject);
+          embedArray.push(createdEmbed);
+        });
+      }
 
       if (message.attachments.size === 1) {
-        const attachement = message.attachments.at(0);
-        if (!attachement) return;
-        if (attachement.contentType && attachement.contentType.match(/jpeg|png|gif|webp/g)) {
+        const attachment = message.attachments.at(0);
+        if (!attachment) return;
+
+        /* Set thumbnail as image content */
+        if (attachment.contentType && attachment.contentType.match(/jpeg|png|gif|webp/g)) {
           Object.defineProperty(logEmbed, "thumbnail", {
-            value: { url: `${attachement.proxyURL}` },
+            value: { url: `${attachment.proxyURL}` },
             writable: false,
             enumerable: true,
           });
-          fieldList.push({ name: "Attachement info", value: `Name : \`${attachement.name}\`` });
+          fieldList.push({
+            name: "Attachement info",
+            value: `Name : [\`${attachment.name}\`](${attachment.url})`,
+          });
         } else {
-          attachementList.push(attachement.url);
-          fieldList.push({ name: "Attachement", value: `\`${attachement.name}\`` });
+          attachementList.push(attachment.url);
+          fieldList.push({ name: "Attachement", value: `[\`${attachment.name}\`](${attachment.url})` });
         }
       } else if (message.attachments.size > 1) {
-        message.attachments.forEach((attachement) => {
-          attachementList.push(attachement.url);
-          fieldList.push({ name: "Attachement", value: `\`${attachement.name}\`${attachement.url}` });
+        let counter = 1;
+        message.attachments.forEach((attachment) => {
+          counter += 1;
+          attachementList.push(attachment.url);
+          fieldList.push({ name: `Attachement ${counter}`, value: `[\`${attachment.name}\`](${attachment.url})` });
         });
       }
 
       const logs = this.client.channels.cache.get(this.logChannelID) as TextChannel;
-      if (!logs) return;
-
-      logs.send({ embeds: [logEmbed], files: attachementList });
+      if ((message.guild as Guild).id === logs.guild.id) {
+        logs.send({ embeds: embedArray, files: attachementList });
+      }
     });
+  }
+
+  /** @description Recrates the embed object from the readonly embedObject */
+  private restructureEmbedObject(embed: APIEmbed) {
+    const constructedEmbedObject = {};
+    Object.entries(embed).forEach(([key, value]) => {
+      if (key === "type") return;
+      Object.defineProperty(constructedEmbedObject, key, {
+        value: value,
+        writable: true,
+        enumerable: true,
+      });
+    });
+    return constructedEmbedObject;
+  }
+
+  /** @description Handles message update, eg : Edit, added Embed etc... */
+  private handleMessageUpdate() {
+    this.client.on(
+      Events.MessageUpdate,
+      /* Use newMessage since it is most recent */
+      (oldMessage: Message<boolean> | PartialMessage, newMessage: Message<boolean> | PartialMessage) => {
+        console.log(oldMessage, newMessage);
+        if (this.logChannelID === "") return;
+
+        const messageAuthor = newMessage.author as User;
+        const messageAuthourProfile = messageAuthor.avatarURL({ size: 64 }) as string;
+        const newMessageID = newMessage.id;
+        const messageURL = newMessage.url;
+        const [oldMessageContent, newMessageContent]: (string | null)[] = [oldMessage.content, newMessage.content];
+        const fieldList: Array<APIEmbedField> = [
+          { name: "Message ID", value: `\`${newMessageID}\``, inline: true },
+          {
+            name: "Message Timestamp",
+            value: `<t:${this.convertSnowFlakeToDate(parseInt(newMessageID, 10))}:F>`,
+            inline: true,
+          },
+          {
+            name: "Message URL",
+            value: `[Message URL](${messageURL})`,
+            inline: true,
+          },
+          {
+            name: "User",
+            value: `\`${messageAuthor.id}\` - <@${messageAuthor.id}>`,
+            inline: true,
+          },
+        ];
+
+        const EMBED_COLOR = 0x2f8bf5;
+        const logEmbed = {
+          author: {
+            name: messageAuthor.globalName ?? messageAuthor.username,
+            icon_url: messageAuthourProfile,
+          },
+          description: `## Old message content\n ${
+            `\`\`\`md\n${oldMessageContent}\`\`\`` ?? "<message content empty>"
+          }\n## New message content\n ${`\`\`\`md\n${newMessageContent}\`\`\`` ?? "<message content empty>"}`,
+          title: `Message edited in <#${newMessage.channel.id}>`,
+          color: EMBED_COLOR,
+          fields: fieldList,
+        } as APIEmbed;
+
+        newMessage.channel.send({ embeds: [logEmbed] });
+      }
+    );
   }
 
   /** @description Handles client login */
@@ -129,13 +229,12 @@ class Bot {
   }
 
   /**
-   * @description Converts snowflake to a date
+   * @description Converts Discord snowflake to a date (Unix seconds)
    * {@link https://discord.com/developers/docs/reference#snowflakes}
    */
-  private convertSnowFlakeToDate(snowflake: number):number {
-    const DISCORD_EPOCH = 1420070400000;
-    const ms = Number(BigInt(snowflake) >> 22n) + DISCORD_EPOCH;
-    return Math.floor(ms/1000);
+  private convertSnowFlakeToDate(snowflake: number): number {
+    const ms = Number(BigInt(snowflake) >> 22n) + this.DISCORD_EPOCH;
+    return Math.floor(ms / 1000);
   }
 }
 
